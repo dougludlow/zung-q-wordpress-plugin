6a97d9780940752ad4845e1a3e53f84cf5de33ad
Douglas Ludlow <dludlow@cityofboise.org>
2014-05-17 21:02:31 -0600

    Added plugin files.

---
+++ zung-q/views/zung.php
@@ -0,0 +1,39 @@
+<div ng-app="ZungApp" ng-controller="ZungController" ng-cloak class="zung ng-cloak">
+  <div class="page-header">
+    <h1>Zung Self-Rating Depression Scale</h1>
+  </div>
+  <table class="table table-striped table-hover table-responsive">
+    <thead>
+      <tr>
+        <th colspan="2">Check the appropriate column</th>
+        <th ng-repeat="response in responses" class="response">{{response}}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr ng-repeat="s in survey">
+        <th>{{$index + 1}}</th>
+        <td>{{s.statement}}</td>
+        <td ng-repeat="score in s.scores" class="response">
+          <input type="radio" name="{{'q' + ($parent.$index + 1)}}" value="{{score}}" ng-model="scores[$parent.$index]"/>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Score</h3>
+    </div>
+    <div class="panel-body">
+      <p>Most people with depression score between <strong>50</strong> and <strong>69</strong>. The highest possible score is <strong>80</strong><a href="#note-1"><sup>1</sup></a>.</p>
+      <p>Your score: <strong>{{score()}}</strong> &nbsp;<span ng-class="range().stateClass" class="badge"><span ng-class="range().iconClass" class="glyphicon"></span> {{range().description}}</span>&nbsp;&nbsp;<a ng-href="{{mailto()}}" ng-show="checks === survey.length" class="send btn btn-xs btn-primary"><span class="glyphicon glyphicon-envelope"></span> Send Score</a></p>
+    </div>
+  </div>
+  <div class="text-muted">
+    <p>Adapted from Zung<a href="#note-2"><sup>2</sup></a>.</p>
+    <p><strong>References: </strong></p>
+    <ol>
+      <li><a id="note-1"></a> Carroll BJ, Fielding JM, Blashki TG. Depression rating scales: a critical review. Arch Gen Psychiatry. 1973; 28:361-366.</li>
+      <li><a id="note-2"></a> Zung WWK. A self-rating depression scale. Arch Gen Psychiatry. 1965; 12:63-70</li>
+    </ol>
+  </div>
+</div>
---
+++ zung-q/zung-q.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package ZungQuestionnaire
+ */
+/*
+Plugin Name: Zung Questionnaire
+Plugin URI: http://codepen.io/dougludlow/pen/aypwg
+Description: The Zung Self-Rating Depression Scale Questionnaire.
+Version: 0.1
+Author: Douglas Ludlow
+Author URI: http://dougludlow.info/
+License: GPLv2 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+define( 'ZUNG__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'ZUNG__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+add_shortcode( 'zung', 'zung' );
+
+//[zung]
+function zung( $atts ){
+
+    wp_enqueue_style( 'bootstrap', '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/css/bootstrap.css' );
+    wp_enqueue_style( 'zung-styles', ZUNG__PLUGIN_URL . 'styles/zung.css' );
+
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'bootstrap', '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.js' );
+    wp_enqueue_script( 'angular', '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.16/angular.js' );
+    wp_enqueue_script( 'jquery.floatThead-slim', 'http://cdnjs.cloudflare.com/ajax/libs/floatthead/1.2.7/jquery.floatThead-slim.js');
+    wp_enqueue_script( 'zung-script', ZUNG__PLUGIN_URL . 'scripts/zung.js' );
+
+    ob_start();
+	include ZUNG__PLUGIN_DIR . 'views/zung.php';
+    return ob_get_clean();
+}
